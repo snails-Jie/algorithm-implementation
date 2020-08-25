3e04d32ab5f15dc0573d40504fe67006c0bf821d
zhangjie <745012773@qq.com>
2020-08-25 21:22:16 +0800

    新增异步变成分支

--- src/main/java/zhangjie/basic/async/spring/UserRegisterCompleteEvent.java
+++
@@ -1,9 +0,0 @@
-package zhangjie.basic.async.spring;
-
-import org.springframework.context.ApplicationEvent;
-
-public class UserRegisterCompleteEvent extends ApplicationEvent {
-    public UserRegisterCompleteEvent(Object source) {
-        super(source);
-    }
-}
